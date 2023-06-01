15c0d72986c487dda7771ef6c580ac7a31d69ca2
matheus-cruz-dev <matheus.cruz.2004@hotmail.com>
2023-05-31 21:38:04 -0300

    Commit

--- src/modulo2/Ex04.java
+++ src/modulo2/Ex04.java
@@ -14,11 +14,7 @@ public class Ex04 {
     System.out.println("Digíte a sua idade: ");
     idade = input.nextInt();
 
-<<<<<<< HEAD
     input.nextLine();
-=======
-    input.nextLine(); // evitar comportamento inesperado de quebra de linha pelo println e nextInt
->>>>>>> 37a946160f0a25692047fc9a97054828511ae8bb
 
     System.out.println("Digíte a sua cidade: ");
     cidade = input.nextLine();
